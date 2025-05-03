87d78e3675e5426c5c012902fff056b57394bb7e
sultanfaathir <sultanfaathir97@gmail.com>
2025-05-03 11:08:28 +0700

    display task

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,12 @@ const addTask = () => {
 <template>
  <input type="text" v-model="newTask" @keyup.enter="addTask">
  <button @click="addTask">Tambahkan</button> 
+
+ <ul>
+  <li v-for="task in tasks" :key="task.id">
+    {{  task.text }}
+  </li>
+ </ul>
 </template>
 
 <style scoped>
