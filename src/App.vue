b7601b40a124a8bc711c7d1e8df8efb39c86e3e5
sultanfaathir <sultanfaathir97@gmail.com>
2025-05-03 11:07:33 +0700

    addTask

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <script setup>
+import { ref } from 'vue'
+
+const tasks = ref([])
+const newTask = ref('')
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push({
+      id: Date.now(),
+      text: newTask.value,
+      completed: false
+    })
+    newTask.value = ''
+  }
+}
+
 </script>
 
 <template>
-  
+ <input type="text" v-model="newTask" @keyup.enter="addTask">
+ <button @click="addTask">Tambahkan</button> 
 </template>
 
 <style scoped>
